3ed0b5d062ead1ba5c12545e06ca724d47e6addc
Kara Beason <karabeason@github.com>
2019-04-03 16:04:04 -0400

    updating python report.

--- classes/task/sentiments_analysis.py
+++ classes/task/sentiments_analysis.py
@@ -16,35 +16,59 @@ if (not os.path.isdir(directory)) or (not os.path.exists(directory)):
     exit(0)
 
 
+def is_new_page(report, y, value, height):
+    if y - 100 < 0:
+        report.showPage()
+        y = height - 100
+        return y
+    else:
+        y = y - value
+        return y
+
+
 def print_report(report_name, sentiments_list):
     save_name = os.path.join(directory, report_name + ".pdf")
     rep = canvas.Canvas(save_name, pagesize=letter)
-    width, height = letter  # keep for later
+    width, height = letter 
     y = height - 100
-    for sentiment in sentiments_list:
-        # Parse file name to get student username
-        
-        rep.drawString(100, y, "Student: ")
+    # Overall Sentiment Analysis First
+    rep.drawString(100, y, "Overall Sentiment: ")
+    for label, val in overall.__dict__.items():
+        y = is_new_page(rep, y, 15, height)
+        rep.drawString(125, y, str(label))
+        y = is_new_page(rep, y, 15, height)
+        if isinstance(val, float):
+            rep.drawString(125, y, str(val))
+    rep.showPage()
+    # Sentiment Analysis by Student
+    for user, sentiment in sentiments_list.iteritems():
+        rep.drawString(100, y, "Student Username: ")
+        rep.drawString(225, y, user)
         for name, value in sentiment.__dict__.items():
-            y = y - 15
+            y = is_new_page(rep, y, 15, height)
             rep.drawString(125, y, str(name))
-            y = y - 15
-            rep.drawString(125, y, str(value))
-        y = y - 100
+            y = is_new_page(rep, y, 15, height)
+            if isinstance(value, float):
+                rep.drawString(125, y, str(value))
+            else:
+                for word in value:
+                    y = is_new_page(rep, y, 15, height)
+                    rep.drawString(125, y, str(word))
     rep.save()
 
 
-sentiments = list()
-
+sentiments = dict()
+overall = ""
 for filename in os.listdir(directory):
     if filename.endswith(".txt"):
-        print(filename)
+        username = filename.split('_')[0]
         filename = os.path.join(directory, filename)
-
         line = ""
         with codecs.open(filename, "r",encoding='utf-8', errors='ignore') as fdata:
             line += fdata.read()
+            overall += line
             line = TextBlob(line)
-            sentiments.append(line.sentiment_assessments)
-
+            sentiments[username] = line.sentiment_assessments
+overall = TextBlob(overall)
+overall_sentiment = overall.sentiment_assessments
 print_report("output", sentiments)
